51c1072c7f60a76a33b1b874e4bb1ff75bdd14ae
Никита Шипилов <websns@mail.ru>
2020-09-01 18:39:06 +0300

    Get keyt and generate options

--- pages/main/accounts/ajax.php
+++ pages/main/accounts/ajax.php
@@ -79,7 +79,7 @@ if (isset($_GET['get_accounts_processing'])) {
 
     $password = encryptPassword($_GET['password'], $transact);
     $params = "ext_transact=$transact&program_sign=$program_sign&program=$program&cabinet_login=$login&dkcp_protocol_version=LAST&lang=ru&password=$password&cmd=getlist_keyt";
-    //$params .= "&login=$login";
+    $params .= "&login=$login";
     $request = "$url/$file?$params";
 
 
